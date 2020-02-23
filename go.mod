840858d024803ef9461568b04f58b24eb5b6a468
hagen1778 <hagen1778@gmail.com>
2020-02-23 16:16:56 +0000

    initial Prometheus processor implementation

--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/cheggaaa/pb/v3 v3.0.4
 	github.com/google/go-cmp v0.4.0 // indirect
 	github.com/influxdata/influxdb v1.7.9
+	github.com/prometheus/prometheus v1.8.2-0.20200110114423-1e64d757f711
 	github.com/urfave/cli/v2 v2.0.0
 )
---
+++ prometheus.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"sync"
+
+	"github.com/cheggaaa/pb/v3"
+	"github.com/prometheus/prometheus/tsdb"
+	"github.com/victoriametrics/vmctl/prometheus"
+	"github.com/victoriametrics/vmctl/vm"
+)
+
+type prometheusProcessor struct {
+	cl *prometheus.Client
+	im *vm.Importer
+	cc int
+}
+
+func (pp *prometheusProcessor) run() error {
+	blocks, err := pp.cl.Explore()
+	if err != nil {
+		return fmt.Errorf("explore failed: %s", err)
+	}
+	if len(blocks) < 1 {
+		return fmt.Errorf("found no blocks to import")
+	}
+
+	question := fmt.Sprintf("Found %d blocks to import. Continue?", len(blocks))
+	if !prompt(question) {
+		return nil
+	}
+
+	bar := pb.StartNew(len(blocks))
+	blockReadersCh := make(chan tsdb.BlockReader)
+	errCh := make(chan error)
+
+	var wg sync.WaitGroup
+	wg.Add(pp.cc)
+	for i := 0; i < pp.cc; i++ {
+		go func() {
+			defer wg.Done()
+			for br := range blockReadersCh {
+				if err := pp.do(br); err != nil {
+					errCh <- fmt.Errorf("request failed for block %q: %s", br.Meta().ULID, err)
+					return
+				}
+				bar.Increment()
+			}
+		}()
+	}
+
+	// any error breaks the import
+	for _, br := range blocks {
+		select {
+		case promErr := <-errCh:
+			return fmt.Errorf("prometheus error: %s", promErr)
+		case vmErr := <-pp.im.Errors():
+			var errTS string
+			for _, ts := range vmErr.Batch {
+				errTS += fmt.Sprintf("%s for timestamps range %d - %d\n",
+					ts.String(), ts.Timestamps[0], ts.Timestamps[len(ts.Timestamps)-1])
+			}
+			return fmt.Errorf("Import process failed for: \n%swith error: %s", errTS, vmErr.Err)
+		case blockReadersCh <- br:
+		}
+	}
+
+	close(blockReadersCh)
+	wg.Wait()
+	pp.im.Close()
+	bar.Finish()
+	log.Println("Import finished!")
+	log.Print(pp.im.Stats())
+	return nil
+}
+
+func (pp *prometheusProcessor) do(b tsdb.BlockReader) error {
+	ss, err := pp.cl.Read(b)
+	if err != nil {
+		return fmt.Errorf("failed to read block: %s", err)
+	}
+	if ss.Err() != nil {
+		return fmt.Errorf("unexpected series set error: %s", err)
+	}
+	for ss.Next() {
+		var name string
+		var labels []vm.LabelPair
+		series := ss.At()
+
+		for _, label := range series.Labels() {
+			if label.Name == "__name__" {
+				name = label.Value
+				continue
+			}
+			labels = append(labels, vm.LabelPair{
+				Name:  label.Name,
+				Value: label.Value,
+			})
+		}
+		if name == "" {
+			return fmt.Errorf("failed to find `__name__` label in labelset for block %v", b.Meta().ULID)
+		}
+
+		var timestamps []int64
+		var values []interface{}
+		it := series.Iterator()
+		for it.Next() {
+			t, v := it.At()
+			timestamps = append(timestamps, t)
+			values = append(values, v)
+		}
+		if it.Err() != nil {
+			return ss.Err()
+		}
+		pp.im.Input() <- &vm.TimeSeries{
+			Name:       name,
+			LabelPairs: labels,
+			Timestamps: timestamps,
+			Values:     values,
+		}
+	}
+	return nil
+}
